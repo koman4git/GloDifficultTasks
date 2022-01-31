8badd5cb14d1b0dfcb3d45ba85d94c22e4cce816
Andrey Lazarew <koman4bk@bk.ru>
2022-01-31 22:56:50 +0300

    сложное ДЗ к уроку 3

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,38 @@
+'use strict';
+//усложненное задание № 1 к уроку № 3
+let lang = prompt("Английский или русский язык?", "en или ru");
+const newArr = [
+  ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"],
+  ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+];
+//вывод через if
+if (lang === "ru") {
+  console.log(newArr[0].join(" "));
+} else if (lang === "en") {
+  console.log(newArr[1].join());
+} else {
+  console.log("Вы не ввели ни одно из предложенных значений! Попробуйте заново");
+}
+//вывод через switch
+switch(true) {
+  case lang === "ru":
+    console.log(newArr[0].join());
+    break;
+  case lang === "en":
+    console.log(newArr[1].join());
+    break;
+  default:
+    console.log("Вы не ввели ни одно из предложенных значений! Попробуйте заново");
+    break;
+}
+//вывод через тернарный оператор
+lang === "ru" ? console.log(newArr[0].join()) : lang === "en" ? console.log(newArr[1].join()) : console.log("Вы не ввели ни одно из предложенных значений! Попробуйте заново")
+
+//усложненное задание № 2 к уроку № 3
+let namePerson= prompt("Введите из предложенного или свой вариант", "Артем, Александр, свой вариант"); 
+namePerson === "Артем" ? console.log("Директор") : namePerson === "Александр" ? console.log("Преподаватель") : console.log("Студент") 
+
+//усложненное задание к уроку № 2
 let num = 266219,
     arr = num.toString().split("");
 
